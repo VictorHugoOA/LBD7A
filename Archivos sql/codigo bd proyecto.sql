c1f9e8009cb011bffe0812aa1630133beed077f0
NRekits <64814187+NRekits@users.noreply.github.com>
2020-11-28 15:29:33 -0600

    Codigos para BD
    Codigos

---
+++ Archivos sql/codigo bd proyecto.sql
@@ -0,0 +1,154 @@
+create database aulavirtualsep;
+use aulavirtualsep;
+
+create table profesor(
+	id varchar(10) not null,
+    nombre varchar(50) not null,
+    apellido_pat varchar(25) not null,
+    apellido_mat varchar(25) not null,
+    correo varchar(50) not null,
+    teléfono numeric(10) not null,
+    sexo char not null,
+    contrasena varchar(15) not null,
+    primary key(id)
+);
+
+create table grupo(
+	id varchar(10) not null,
+    grado int not null,
+    clase char not null,
+    id_profesor varchar(10),
+    primary key(id),
+    foreign key(id_profesor) references profesor(id)
+    
+);
+
+create table alumno(
+	id varchar(10) not null,
+    nombre varchar(50) not null,
+    apellido_pat varchar(25) not null,
+    apellido_mat varchar(25) not null,
+    sexo char not null,
+    id_grupo varchar(10) not null,
+    primary key(id),
+	contrasena varchar(15) not null,
+    foreign key(id_grupo) references grupo(id)
+);
+
+create table materia(
+	id varchar(10) not null,
+    nombre varchar(50) not null,
+    campo varchar(100) not null,
+    primary key(id)
+);
+
+create table clases_de(
+	id_grupo varchar(10) not null,
+    id_materia varchar(10) not null,
+    primary key(id_grupo, id_materia),
+    foreign key(id_grupo) references grupo(id),
+    foreign key(id_materia) references materia(id)
+);
+
+create table actividad(
+	id numeric(10) not null,
+    titulo varchar(30) not null,
+    fecha_limite date not null,
+    hora_limite time not null,
+    descripción text not null,
+	estado int default 0,
+    retraso int not null,
+    id_materia varchar(10) not null,
+    primary key(id),
+    foreign key(id_materia) references materia(id)
+);
+
+create table realiza
+(
+	id_alumno varchar(10) not null,
+    id_actividad numeric(10) not null,
+    fecha_entrega date,
+    hora_entrega time,
+    calificacion float,
+	estado_entrega int default 0,
+    primary key(id_alumno, id_actividad),
+    foreign key(id_alumno) references alumno(id),
+    foreign key(id_actividad) references actividad(id)
+);
+
+create table libro (
+	id varchar(10) not null,
+    año int not null,
+    editorial varchar(50) not null,
+    título varchar(50) not null,
+    id_materia varchar(10) not null,
+    primary key(id),
+    foreign key(id_materia) references materia(id)
+);
+
+create table tutoría(
+	id varchar(10) not null,
+    titulo varchar(30) not null,
+    descripción text not null,
+    primary key(id)
+);
+
+create table pide(
+	id_tutoría varchar(10) not null,
+	id_alumno varchar(10) not null,
+	fecha date not null,
+    primary key(id_tutoría, id_alumno),
+    foreign key(id_tutoría) references tutoría(id),
+    foreign key(id_alumno) references alumno(id)
+);
+
+create table imparte(
+	id_tutoría varchar(10) not null,
+    id_profesor varchar(10),
+    primary key(id_tutoría, id_profesor),
+    foreign key(id_tutoría) references tutoría(id),
+    foreign key(id_profesor) references profesor(id)
+);
+
+/*vistas*/
+create view alumnosact as select R.id_alumno, R.id_actividad,A.id_materia from
+realiza R left join actividad A on R.id_actividad=A.id;
+create view LoginA as select id,contrasena from alumno;
+create view LoginP as select id,contrasena from profesor;
+create view listaalumnos as select A.* ,G.id_profesor from 
+alumno A left join grupo G on A.id_grupo=G.id;
+
+/*Creación del procedimiento para login_alumno*/
+DELIMITER //
+create procedure aulavirtualsep.login_alumno (in userid varchar(10), in pass varchar(15))
+begin
+	select id from logina where id = userid and contrasena = pass;
+end//
+DELIMITER ;
+
+/*Creación del procedimiento para login_profesor*/
+DELIMITER //
+create procedure aulavirtualsep.login_profesor (in userid varchar(10), in pass varchar(15))
+begin
+	select id from loginp where id = userid and contrasena = pass;
+end//
+DELIMITER ;
+
+/*Ejemplos para ejecutar los procedimientos para el login
+call login_alumno("a000001", "c000003");
+call login_profesor("p000001", "c000001");*/
+
+/*Creación de procedimiento para los avances de los alumnos*/
+DELIMITER //
+create procedure aulavirtualsep.avances (in id varchar(10), out total float)
+begin
+	declare a float default 0;
+    declare b float default 0;
+	set a = (select count(id_actividad) from realiza where id_alumno = id group by(id_alumno));
+    set b = (select count(id_actividad) from realiza where id_alumno = id and fecha_entrega is not null group by(id_alumno));
+	set total = @a/@b;
+end//
+DELIMITER ;
+
+/*Ejemplo para ejecutar el procedimiento para avances
+call avances("a000001", @out); select @out;*/
